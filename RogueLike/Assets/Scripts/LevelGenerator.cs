8c63d17ac1ac31dd4d22a4e97c4d295cf1b0751e
Álvaro Mendoza Gonzalez <alvaro.men98@gmail.com>
2020-04-22 14:41:09 +0200

    Disparamos una esfera con las flechas

--- RogueLike/Assets/Scripts/LevelGenerator.cs
+++ RogueLike/Assets/Scripts/LevelGenerator.cs
@@ -5,289 +5,290 @@ using UnityEngine.SceneManagement;
 
 public class LevelGenerator : MonoBehaviour
 {
-	//enum GridSpace { empty, floor, floorAlt1, floorAlt2, floorAlt3, wallLeft, wallRight, corner1, corner2, corner3, corner4 };
-	enum GridSpace {empty, floor, wall, init, passage};
+    //enum GridSpace { empty, floor, floorAlt1, floorAlt2, floorAlt3, wallLeft, wallRight, corner1, corner2, corner3, corner4 };
+    enum GridSpace { empty, floor, wall, init, passage };
 
-	Dictionary<GridSpace, GameObject> mapValues;
-	
-	GridSpace[,] grid;
-	int floorHeight, floorWidth;
-	Vector2 roomSizeWorldUnits;
-	float worldUnitsInOneGridCell = 1;
+    Dictionary<GridSpace, GameObject> mapValues;
 
-
-	struct Walker
-	{
-		public Vector2 dir;
-		public Vector2 pos;
-	}
-
-	//public float percentToFill = .6f;
-	
-	public int maxPathways = 40;
-	public int maxRooms = 100;
-	public int maxTries = 50;
+    GridSpace[,] grid;
+    int floorHeight, floorWidth;
+    Vector2 roomSizeWorldUnits;
+    float worldUnitsInOneGridCell = 1;
 
 
-	struct Room
-	{
-		public Vector2 init;
-		public Vector2 dims;
-	}
+    struct Walker
+    {
+        public Vector2 dir;
+        public Vector2 pos;
+    }
 
-	List<Room> rooms;
+    //public float percentToFill = .6f;
 
-	public GameObject wallObj, floorObj, pathObj, emptyObj;
+    public int maxPathways = 40;
+    public int maxRooms = 100;
+    public int maxTries = 50;
 
-	Transform boardHolder;
 
-	void Start()
-	{
-		Setup();
-		CreateRooms();
-		CreateWalkWays();
-		SpawnLevel();
-	}
+    struct Room
+    {
+        public Vector2 init;
+        public Vector2 dims;
+    }
 
-	private void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			SceneManager.LoadScene(0);
-		}
-	}
+    List<Room> rooms;
 
-	void Setup()
-	{
-		roomSizeWorldUnits = new Vector2(Random.Range(100, 200), Random.Range(100, 200));
+    public GameObject wallObj, floorObj, pathObj, emptyObj;
 
-		boardHolder = new GameObject("Board").transform;
+    Transform boardHolder;
 
-		//find grid size
-		floorHeight = Mathf.RoundToInt(roomSizeWorldUnits.x / worldUnitsInOneGridCell);
-		floorWidth = Mathf.RoundToInt(roomSizeWorldUnits.y / worldUnitsInOneGridCell);
-		//create grid
-		grid = new GridSpace[floorWidth, floorHeight];
-		//set grid's default state
-		for (int x = 0; x < floorWidth - 1; x++)
-		{
-			for (int y = 0; y < floorHeight - 1; y++)
-			{
-				//make every cell "empty"
-				grid[x, y] = GridSpace.empty;
-			}
-		}
+    void Start()
+    {
+        Setup();
+        CreateRooms();
+        CreateWalkWays();
+        SpawnLevel();
+    }
 
-		rooms = new List<Room>();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 
-		InitMapValues();
-	}
+    void Setup()
+    {
+        roomSizeWorldUnits = new Vector2(Random.Range(100, 200), Random.Range(100, 200));
 
-	void InitMapValues()
-	{
-		mapValues = new Dictionary<GridSpace, GameObject>();
+        boardHolder = new GameObject("Board").transform;
 
-		mapValues[GridSpace.floor] = floorObj;
-		mapValues[GridSpace.passage] = pathObj;
-		mapValues[GridSpace.wall] = emptyObj;
-		mapValues[GridSpace.empty] = emptyObj;
-		mapValues[GridSpace.init] = wallObj;
-	}
+        //find grid size
+        floorHeight = Mathf.RoundToInt(roomSizeWorldUnits.x / worldUnitsInOneGridCell);
+        floorWidth = Mathf.RoundToInt(roomSizeWorldUnits.y / worldUnitsInOneGridCell);
+        //create grid
+        grid = new GridSpace[floorWidth, floorHeight];
+        //set grid's default state
+        for (int x = 0; x < floorWidth - 1; x++)
+        {
+            for (int y = 0; y < floorHeight - 1; y++)
+            {
+                //make every cell "empty"
+                grid[x, y] = GridSpace.empty;
+            }
+        }
 
-	void CreateRooms()
-	{
-		for (int r = 0; r < maxRooms; r++)
-		{
-			int x;
-			int y;
+        rooms = new List<Room>();
 
-			int maxRoomWidth;
-			int maxRoomHeight;
+        InitMapValues();
+    }
 
-			Room currRoom = new Room();
+    void InitMapValues()
+    {
+        mapValues = new Dictionary<GridSpace, GameObject>();
 
-			bool overlap = false;
-			int numTries = 0;
-			do
-			{
-				numTries++;
-				do
-				{
-					x = Random.Range(5, floorWidth - 5);
-					y = Random.Range(5, floorHeight - 5);
-				} while (grid[x, y] != GridSpace.empty);
-				
-				maxRoomWidth = Random.Range(5, 15);
-				maxRoomHeight = Random.Range(5, 15);
+        mapValues[GridSpace.floor] = floorObj;
+        mapValues[GridSpace.passage] = pathObj;
+        mapValues[GridSpace.wall] = emptyObj;
+        mapValues[GridSpace.empty] = emptyObj;
+        mapValues[GridSpace.init] = wallObj;
+    }
 
-				currRoom.init = new Vector2(x, y);
-				currRoom.dims = new Vector2(maxRoomWidth, maxRoomHeight);
+    void CreateRooms()
+    {
+        for (int r = 0; r < maxRooms; r++)
+        {
+            int x;
+            int y;
 
-				if (rooms.Count > 0)
-				{
-					foreach (Room room in rooms)
-					{
-						bool ovv = doOverlap(currRoom, room, 2);
-						if (ovv)
-						{
-							overlap = true;
-							break;
-						}
-					}
-				}
-			} while (overlap && (numTries <= maxTries));
+            int maxRoomWidth;
+            int maxRoomHeight;
 
-			if (!overlap)
-			{
-				rooms.Add(currRoom);
+            Room currRoom = new Room();
 
-				for (int i = x; i < x + maxRoomWidth; i++)
-				{
-					for (int j = y; j < y + maxRoomHeight; j++)
-					{
-						int iInRange = Mathf.Clamp(i, 1, floorWidth - 3);
-						int jInRange = Mathf.Clamp(j, 1, floorHeight - 3);
+            bool overlap = false;
+            int numTries = 0;
+            do
+            {
+                numTries++;
+                do
+                {
+                    x = Random.Range(5, floorWidth - 5);
+                    y = Random.Range(5, floorHeight - 5);
+                } while (grid[x, y] != GridSpace.empty);
 
-						if (!(grid[iInRange, jInRange] == GridSpace.floor))
-							grid[iInRange, jInRange] = GridSpace.floor;
-						else
-							grid[iInRange, jInRange] = GridSpace.empty;
+                maxRoomWidth = Random.Range(5, 15);
+                maxRoomHeight = Random.Range(5, 15);
 
-						grid[x, y] = GridSpace.init;
-					}
-				}
-			}
-		}
-	}
+                currRoom.init = new Vector2(x, y);
+                currRoom.dims = new Vector2(maxRoomWidth, maxRoomHeight);
 
-	void CreateWalkWays()
-	{
-		for (int i = 0; i < maxPathways; i++)
-		{
-			int randInitIndex = Random.Range(0, rooms.Count - 1);
-			int randEndIndex;
+                if (rooms.Count > 0)
+                {
+                    foreach (Room room in rooms)
+                    {
+                        bool ovv = doOverlap(currRoom, room, 2);
+                        if (ovv)
+                        {
+                            overlap = true;
+                            break;
+                        }
+                    }
+                }
+            } while (overlap && (numTries <= maxTries));
 
-			do
-			{
-				randEndIndex = Random.Range(0, rooms.Count - 1);
-			} while (randEndIndex == randInitIndex);
+            if (!overlap)
+            {
+                rooms.Add(currRoom);
 
-			// we have our starting and ending rooms
-			Room initialRoom = rooms[randInitIndex];
-			Room finalRoom = rooms[randEndIndex];
+                for (int i = x; i < x + maxRoomWidth; i++)
+                {
+                    for (int j = y; j < y + maxRoomHeight; j++)
+                    {
+                        int iInRange = Mathf.Clamp(i, 1, floorWidth - 3);
+                        int jInRange = Mathf.Clamp(j, 1, floorHeight - 3);
 
-			// more precisely, we have our starting and ending points
-			int randInitX = Random.Range((int)initialRoom.init.x, (int)(initialRoom.init.x + initialRoom.dims.x));
-			int randInitY = Random.Range((int)initialRoom.init.y, (int)(initialRoom.init.y + initialRoom.dims.y));
+                        if (!(grid[iInRange, jInRange] == GridSpace.floor))
+                            grid[iInRange, jInRange] = GridSpace.floor;
+                        else
+                            grid[iInRange, jInRange] = GridSpace.empty;
 
-			int randEndX = Random.Range((int)finalRoom.init.x, (int)(finalRoom.init.x + finalRoom.dims.x));
-			int randEndY = Random.Range((int)finalRoom.init.y, (int)(finalRoom.init.y + finalRoom.dims.y));
+                        grid[x, y] = GridSpace.init;
+                    }
+                }
+            }
+        }
+    }
 
-			print("init: " + randInitX + ", " + randInitY + ". end: " + randEndX + ", " + randEndY);
+    void CreateWalkWays()
+    {
+        for (int i = 0; i < maxPathways; i++)
+        {
+            int randInitIndex = Random.Range(0, rooms.Count - 1);
+            int randEndIndex;
 
-			// we spawn at the beginning
-			Vector2 spawnPos = new Vector2(randInitX, randInitY);
-			Walker walker = new Walker
-			{
-				pos = spawnPos
-			};
+            do
+            {
+                randEndIndex = Random.Range(0, rooms.Count - 1);
+            } while (randEndIndex == randInitIndex);
 
-			int diffX = (int)walker.pos.x - randEndX;
-			int diffY = (int)walker.pos.y - randEndY;
+            // we have our starting and ending rooms
+            Room initialRoom = rooms[randInitIndex];
+            Room finalRoom = rooms[randEndIndex];
 
-			while (Mathf.Abs(diffX) > 0) //difference is growing instead of diminishing, FIX
-			{
-				print("going for x");
-				int xInRange = (int)Mathf.Clamp(walker.pos.x, 1, floorWidth - 2);
-				int yInRange = (int)Mathf.Clamp(walker.pos.y, 1, floorHeight - 2);
-				if (grid[xInRange, yInRange] != GridSpace.floor)
-				{
-					grid[xInRange, yInRange] = GridSpace.passage;
-				}
-				walker.pos.x -= Mathf.Sign(diffX);
-				diffX = (int)walker.pos.x - randEndX;
-			}
+            // more precisely, we have our starting and ending points
+            int randInitX = Random.Range((int)initialRoom.init.x, (int)(initialRoom.init.x + initialRoom.dims.x));
+            int randInitY = Random.Range((int)initialRoom.init.y, (int)(initialRoom.init.y + initialRoom.dims.y));
 
-			while (Mathf.Abs(diffY) > 0)
-			{
-				print("going for y");
-				int xInRange = (int)Mathf.Clamp(walker.pos.x, 1, floorWidth - 2);
-				int yInRange = (int)Mathf.Clamp(walker.pos.y, 1, floorHeight - 2);
-				if (grid[xInRange, yInRange] != GridSpace.floor)
-				{
-					grid[xInRange, yInRange] = GridSpace.passage;
-				}
-				walker.pos.y -= Mathf.Sign(diffY);
-				diffY = (int)walker.pos.y - randEndY;
-			}
-		}
-	}
+            int randEndX = Random.Range((int)finalRoom.init.x, (int)(finalRoom.init.x + finalRoom.dims.x));
+            int randEndY = Random.Range((int)finalRoom.init.y, (int)(finalRoom.init.y + finalRoom.dims.y));
 
-	bool doOverlap(Room room1, Room room2, int margin)
-	{
-		Vector2 l1 = room1.init;
-		Vector2 r1 = room1.init + room1.dims;
+            print("init: " + randInitX + ", " + randInitY + ". end: " + randEndX + ", " + randEndY);
 
-		Vector2 l2 = room2.init;
-		Vector2 r2 = room2.init + room2.dims;
+            // we spawn at the beginning
+            Vector2 spawnPos = new Vector2(randInitX, randInitY);
+            Walker walker = new Walker
+            {
+                pos = spawnPos
+            };
 
-		
+            int diffX = (int)walker.pos.x - randEndX;
+            int diffY = (int)walker.pos.y - randEndY;
 
-		if(r1.x + margin < l2.x || r1.y + margin < l2.y || l1.x - margin > r2.x || l1.y - margin > r2.y)
-		{
-			return false;
-		}
+            while (Mathf.Abs(diffX) > 0) //difference is growing instead of diminishing, FIX
+            {
+                print("going for x");
+                int xInRange = (int)Mathf.Clamp(walker.pos.x, 1, floorWidth - 2);
+                int yInRange = (int)Mathf.Clamp(walker.pos.y, 1, floorHeight - 2);
+                if (grid[xInRange, yInRange] != GridSpace.floor)
+                {
+                    grid[xInRange, yInRange] = GridSpace.passage;
+                }
+                walker.pos.x -= Mathf.Sign(diffX);
+                diffX = (int)walker.pos.x - randEndX;
+            }
 
-		return true;
-	}
+            while (Mathf.Abs(diffY) > 0)
+            {
+                print("going for y");
+                int xInRange = (int)Mathf.Clamp(walker.pos.x, 1, floorWidth - 2);
+                int yInRange = (int)Mathf.Clamp(walker.pos.y, 1, floorHeight - 2);
+                if (grid[xInRange, yInRange] != GridSpace.floor)
+                {
+                    grid[xInRange, yInRange] = GridSpace.passage;
+                }
+                walker.pos.y -= Mathf.Sign(diffY);
+                diffY = (int)walker.pos.y - randEndY;
+            }
+        }
+    }
 
-	int NumberOfFloors()
-	{
-		int count = 0;
-		foreach (GridSpace space in grid)
-		{
-			if (space == GridSpace.floor)
-			{
-				count++;
-			}
-		}
-		return count;
-	}
+    bool doOverlap(Room room1, Room room2, int margin)
+    {
+        Vector2 l1 = room1.init;
+        Vector2 r1 = room1.init + room1.dims;
 
-	Vector2 RandomDirection()
-	{
-		//pick random int between 0 and 3
-		int choice = Mathf.FloorToInt(Random.value * 3.99f);
-		//use that int to chose a direction
-		switch (choice)
-		{
-			case 0:
-				return Vector2.down;
-			case 1:
-				return Vector2.left;
-			case 2:
-				return Vector2.up;
-			default:
-				return Vector2.right;
-		}
-	}
+        Vector2 l2 = room2.init;
+        Vector2 r2 = room2.init + room2.dims;
 
-	void SpawnLevel()
-	{
-		for (int x = 0; x < floorWidth; x++)
-		{
-			for (int y = 0; y < floorHeight; y++)
-			{
-				Spawn(x, y, mapValues[grid[x, y]]);
-			}
-		}
-	}
-	
-	void Spawn(float x, float y, GameObject toSpawn)
-	{
-		//find the position to spawn
-		Vector2 offset = roomSizeWorldUnits / 2.0f;
-		Vector2 spawnPos = new Vector2(x, y) * worldUnitsInOneGridCell - offset;
-		//spawn object
-		Instantiate(toSpawn, spawnPos, Quaternion.identity).transform.SetParent(boardHolder);
-	}
+
+
+        if (r1.x + margin < l2.x || r1.y + margin < l2.y || l1.x - margin > r2.x || l1.y - margin > r2.y)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    int NumberOfFloors()
+    {
+        int count = 0;
+        foreach (GridSpace space in grid)
+        {
+            if (space == GridSpace.floor)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    Vector2 RandomDirection()
+    {
+        //pick random int between 0 and 3
+        int choice = Mathf.FloorToInt(Random.value * 3.99f);
+        //use that int to chose a direction
+        switch (choice)
+        {
+            case 0:
+                return Vector2.down;
+            case 1:
+                return Vector2.left;
+            case 2:
+                return Vector2.up;
+            default:
+                return Vector2.right;
+        }
+    }
+
+    void SpawnLevel()
+    {
+        for (int x = 0; x < floorWidth; x++)
+        {
+            for (int y = 0; y < floorHeight; y++)
+            {
+                Spawn(x, y, mapValues[grid[x, y]]);
+            }
+        }
+    }
+
+    void Spawn(float x, float y, GameObject toSpawn)
+    {
+        //find the position to spawn
+        Vector2 offset = roomSizeWorldUnits / 2.0f;
+        Vector2 spawnPos = new Vector2(x, y) * worldUnitsInOneGridCell - offset;
+        //spawn object
+        Instantiate(toSpawn, spawnPos, Quaternion.identity).transform.SetParent(boardHolder);
+    }
+}
--- RogueLike/Assets/Scripts/PlayerController.cs
+++ RogueLike/Assets/Scripts/PlayerController.cs
@@ -10,11 +10,18 @@ public class PlayerController : MonoBehaviour
     public Transform groundPos;
     public float checkRadius;
     public LayerMask whatIsGround;
+    public GameObject bulletPrefab;
+
+    private bool isShooting;
+    private float fireRate;
 
     private void Start()
     {
         //anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        isShooting = false;
+        fireRate = 0.5f;
     }
 
     private void Update()
@@ -41,6 +48,82 @@ public class PlayerController : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
+        if (Input.GetKey("up"))
+        {
+            if (!isShooting)
+            {
+                isShooting = true;
+                StartCoroutine(Shoot("up"));
+            }
+        }
+        if (Input.GetKey("down"))
+        {
+            if (!isShooting)
+            {
+                isShooting = true;
+                StartCoroutine(Shoot("down"));
+            }
+        }
+        if (Input.GetKey("left"))
+        {
+            if (!isShooting)
+            {
+                isShooting = true;
+                StartCoroutine(Shoot("left"));
+            }
+        }
+        if (Input.GetKey("right"))
+        {
+            if (!isShooting)
+            {
+                isShooting = true;
+                StartCoroutine(Shoot("right"));
+            }
+        }
+    }
+
+    /**
+     * Creates a bullet and apply it a force in order to move it in the direction given by key
+     */
+    private IEnumerator Shoot(string key)
+    {
+
+        Quaternion q = Quaternion.identity;
+        Vector3 pos = transform.position;
+        Vector3 force = new Vector3(0, 0, 0);
+
+        switch (key)
+        {
+            //The quaternion for the prefab is taking into account that the bullet originally is looking to the right
+            case "up":
+                q[0] = 90;
+                pos.y += 0.5f; // displace the bullet a little bit to appear above of the player
+                force.y += 10;
+                break;
+            case "down":
+                q[0] = -90;
+                pos.y += -0.5f; // displace the bullet a little bit to appear below of the player
+                force.y -= 10;
+                break;
+            case "left":
+                q[1] = 180;
+                pos.x += -0.5f; // displace the bullet a little bit to appear at the left of the player
+                force.x -= 10;
+                break;
+            case "right":
+                pos.x += 0.5f; // displace the bullet a little bit to appear at the right of the player
+                force.x += 10;
+                break;
+            default:
+                break;
+        }
+
+        GameObject bullet = Instantiate(bulletPrefab, pos, q);
+        bullet.GetComponent<Rigidbody2D>().AddForce(force);
+
+        yield return new WaitForSeconds(fireRate);
+
+        isShooting = false;
     }
     
 }
